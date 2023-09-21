61c509e9f264c73e6bfbec3a5669df460b9b33af
newsuperfi <122774009+newsuperfi@users.noreply.github.com>
2023-09-21 18:09:24 +0900

    fix/auth 토큰 추출위치 cookie로 수정

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    credentials: true,
+  });
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
